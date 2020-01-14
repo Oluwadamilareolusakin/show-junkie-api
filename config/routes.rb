22132730428b8f03b5fe452bda74bc28a77275f6
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 06:39:20 -0600

    Write routes for comments and enquiries

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get 'users/new'
+
+  get 'users/enquiries', to: 'enquiries#index'
+  post 'users/:users_id/enquiries', to: 'enquiries#create'
+  
+  get '/shows/:show/comments', to: 'comments#index'
+  post '/shows/:show/comments', to: 'comments#create'
+  delete '/shows/:show/comments/:id', to: 'comments#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
