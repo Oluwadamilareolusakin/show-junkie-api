db112248f4149e65db65e40d3921404742469d6b
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 13:07:34 -0600

    Write favourties controller

---
+++ app/controllers/favourites_controller.rb
@@ -0,0 +1,23 @@
+class FavouritesController < ApplicationController
+  before_action :set_favourite, only: %i[ destroy ]                                                 
+
+  def index
+    @favourites = Favourites.where(user_id: (params[:user_id]).to_i)
+    json_response(favourites)
+  end
+
+  def create
+    @favourite = Favourite.create!(favourite_params)
+  end
+
+  def destroy
+    @favourite.destroy
+    head :no_content
+  end
+
+  private
+    def set_favourite
+      @favourites = Favourite.where(user_id: (params[:user_id]).to_i) 
+      @favourite = @favourites.find_by(id: (params[:id]).to_i)
+    end
+end
