10d450eb5d3eafdaa5156c800925ed4ff73bd713
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 06:42:36 -0600

    Write enquiry API tests

---
+++ spec/requests/enquiry_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Enquiry API', type: :request do
+  let!(:enquiries) { create_list(:enquiry, 10) }
+  let(:user) { create(:user) }
+  let(:user_id) { user.id }
+
+  describe 'get /users/enquries' do
+    before { get '/users/enquiries' }
+
+    it 'should return all enquiries' do
+      expect(json.size).to eq(10)
+      expect(json).to_not be_empty
+    end
+
+    it 'should return status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+  
+  describe 'post /users/:user_id/enquiries' do
+    let(:valid_attributes) { { user_id: user.id, message: 'This is a sample enquiry' } }
+
+    context 'valid enquiry details' do
+      before {post "/users/#{user_id}/enquiries", params: valid_attributes}
+
+      it 'should create an enquiry' do
+        expect(json['message']).to eq('This is a sample enquiry')
+      end
+
+      it 'should return status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+  end
+end
