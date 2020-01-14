a28ec1c1a5171d3d0a70ae931a9e7f342213333c
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 06:40:45 -0600

    Write user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should have_many(:comments) }
+  it { should have_many(:enquiries) }
+  it { should have_many(:suggestions) }
 end
