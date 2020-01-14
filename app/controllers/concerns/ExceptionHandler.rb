74656068d12ef3997341156f1488ff8730ad40cd
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 11:44:50 -0600

    Update exception handler

--- app/controllers/concerns/ExceptionHandler.rb
+++ app/controllers/concerns/ExceptionHandler.rb
@@ -1,7 +1,16 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  class InvalidToken < StandardError end;
+  class MissingToken < StandardError end;
+  class AuthenticationError < StandardError end;
+
   included do
+    rescue from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue from ExceptionHandler::AuthenticationError, with: :four_zero_one
+
     rescue_from ActiveRecord::RecordNotFound do |error|
       json_response({message: error.message}, :not_found)
     end
@@ -9,5 +18,14 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordInvalid do |error|
       json_response({message: error.message}, :unprocessable_entity)  
     end
+  end
+
+  def four_twenty_two(error)
+    json_response({message: error.message}, :unprocessable_entity)
+  end
+
+
+  def four_twenty_two(error)
+    json_response({message: error.message}, :unauthorized)
   end
 end
