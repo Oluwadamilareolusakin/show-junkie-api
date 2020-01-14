6b8d5f59258ae714a32aad071ccc87a28f9f44db
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 08:10:19 -0600

    Write suggestions controller

---
+++ app/controllers/suggestions_controller.rb
@@ -0,0 +1,17 @@
+class SuggestionsController < ApplicationController
+
+  def index
+    @suggestions = Suggestion.all
+    json_response(@suggestions)
+  end
+
+  def create
+    @suggestion = Suggestion.create!(suggestion_params) 
+    json_response(@suggestion, :created)
+  end
+
+  private
+    def suggestion_params
+      params.permit(:message, :user_id)
+    end
+end
