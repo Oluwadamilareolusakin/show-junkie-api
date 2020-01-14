29495cbf2bc3c70dde1b7bff68ed29caa9b44d92
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 06:35:34 -0600

    Write comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[ destroy ]
+
+  def index
+    @comments = Comment.where(show: (params[:show]).to_i)
+    json_response(@comments)
+  end
+
+  def create
+    @comment = Comment.create!(comment_params)
+    json_response(@comment, :created)
+  end
+  
+  def destroy
+    @comment.destroy
+  end
+
+  private
+    def set_comment
+      @comments = Comment.where(show: (params[:show]).to_i)
+      @comment = @comments.find_by(id: params[:id])
+    end
+
+    def comment_params
+      params.permit(:content, :show, :user_id)
+    end
 end
