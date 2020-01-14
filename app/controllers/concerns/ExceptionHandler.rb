59cb0a3a8cbbbc6ca350a5657fe2825b39986287
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 13:06:50 -0600

    Correct rescue_from systax error

--- app/controllers/concerns/ExceptionHandler.rb
+++ app/controllers/concerns/ExceptionHandler.rb
@@ -1,15 +1,15 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
-  class InvalidToken < StandardError end;
-  class MissingToken < StandardError end;
-  class AuthenticationError < StandardError end;
+  class InvalidToken < StandardError; end
+  class MissingToken < StandardError; end
+  class AuthenticationError < StandardError; end
 
   included do
-    rescue from ActiveRecord::RecordInvalid, with: :four_twenty_two
-    rescue from ExceptionHandler::InvalidToken, with: :four_twenty_two
-    rescue from ExceptionHandler::MissingToken, with: :four_twenty_two
-    rescue from ExceptionHandler::AuthenticationError, with: :four_zero_one
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :four_zero_one
 
     rescue_from ActiveRecord::RecordNotFound do |error|
       json_response({message: error.message}, :not_found)
@@ -20,12 +20,13 @@ module ExceptionHandler
     end
   end
 
+  private
   def four_twenty_two(error)
     json_response({message: error.message}, :unprocessable_entity)
   end
 
 
-  def four_twenty_two(error)
+  def four_zero_one(error)
     json_response({message: error.message}, :unauthorized)
   end
 end
