b232fafa75466d7621f46cbe47112d894ae8f15f
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 06:41:15 -0600

    Write enquiry model tests

--- spec/models/enquiry_spec.rb
+++ spec/models/enquiry_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Enquiry, type: :model do
-  it { should belong_to(:user) }
   it { should validate_presence_of(:message) }
+  it { should belong_to(:user) }
 end
