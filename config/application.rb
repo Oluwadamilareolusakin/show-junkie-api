a0086b0ca58f6e43deff770070bd0d9ec543036e
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 18:07:49 -0600

    Allow CORS from frontend site

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module ShowJunkieApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://showjunkie.herokuapp.com',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
