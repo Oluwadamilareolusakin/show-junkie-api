745623027991ab2e3daa2ae0aad1a6e03f271ad5
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-18 02:32:41 -0600

    Add access control allow origin header in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
     resource '*',
-      headers: :any,
+      headers: :any,'Access-Control-Allow-Origin',
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
