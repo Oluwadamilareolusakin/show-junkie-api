c9d20c32a820deec4c622662575df50d5a28c3af
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-17 19:55:34 -0600

    Enter secret into jwt singleton manually for test

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -8,7 +8,8 @@ class JsonWebToken
     # set expiry to 24 hours from creation time
     payload[:exp] = exp.to_i
     # sign token with application secret
-    JWT.encode(payload, HMAC_SECRET)
+    p "I AM: #{HMAC_SECRET}"
+    JWT.encode(payload, 123909EI9E82930UEJE1E787612EGUW)
   end
 
   def self.decode(token)
